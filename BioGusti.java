ad7d29588f8133915ec17ebe31faee3ceaedb4d6
Hadhratin Agusti Susanto <91587930+hdrgusti@users.noreply.github.com>
2021-10-12 20:26:02 +0700

    Java Challenges 1
    Challenges 1

---
+++ BioGusti.java
@@ -0,0 +1,21 @@
+public class BioGusti {
+    public static void main(String[] args){
+        String firstName = " Hadhratin";
+        String thirdName = " Agusti";
+        String lastName = " Susanto";
+        int number = 22;
+        String address = " Komplek Permata Hijau";
+        String phonenumber2 = " 081362973144";
+        String hobby = " Reading";
+        String favoriteFood = " Sate";
+        double floatNumber =  10.30;
+
+         System.out.println("Name :" + firstName + thirdName + lastName);
+         System.out.println("Age :" + number);
+         System.out.println("MyAddress :" + address);
+         System.out.println("Phone Number :" + phonenumber2);
+         System.out.println("Hobby :" + hobby);
+         System.out.println("Favorite Food :" + favoriteFood);
+         System.out.println("Time :" + floatNumber);
+    }
+}
